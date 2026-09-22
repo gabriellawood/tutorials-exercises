584113ab31759cdb5d17c4b16a4f0126be415b0c
gabriellawood <gwood@udel.edu>
2026-09-22 11:20:40 -0400

    week-03 excersise complete

--- week-03/exercises/exercise-03-template.R
+++ week-03/exercises/exercise-03-template.R
@@ -26,9 +26,9 @@
 	# Create a path to the folder containing the two CSV files.
 	
 	data_file_path <- here(
-	  "_______",
-	  "_______",
-	  "_______")
+	  "week-03",
+	  "exercises",
+	  "data")
 	
 	
 	# Import both files.
@@ -36,35 +36,36 @@
 	
 	upper_bay <- read_csv(
 	  file.path(
-	    ____________________,
-	    "___________________"
+	    data_file_path,
+	    "upper-bay-fish-counts.csv"
 	  ),
 	  na = c("", "NA", "."))
 	
 	lower_bay <- read_csv(
 	  file.path(
-	    ____________________,
+	    data_file_path,
 	    "lower-bay-fish-counts.csv"
 	  ),
-	  na = c(__, __, __))
+	  na = c("", "NA", "."))
 	
 	
 	# Inspect both data frames.
 	
-	glimpse(____________________)
-	glimpse(____________________)
+	glimpse(lower_bay)
+	glimpse(upper_bay)
 	
-	names(____________________)
-	names(____________________)
+	names(lower_bay)
+	names(upper_bay)
 	
 	
 	# Answer in comments:
 	#
 	# 1. Do the two files contain the same variables?
-	#
+	#Yes, the files contain the same variables.
 	# 2. What does one row currently represent?
-	#
+	#One row represents the amount of types of fish measured in the lower bay on a certain site during certain year. 
 	# 3. Which column names appear to contain more than one variable?
+	#The fish columns contain two variables, the species and the a or j on the end.
 	
 	
 	
@@ -77,8 +78,8 @@
 	# different regions. Combine them into one data frame named fish_wide.
 	
 	fish_wide <- bind_rows(
-	  ____________________,
-	  ____________________
+	  upper_bay,
+	  lower_bay
 	)
 	
 	fish_wide
@@ -86,13 +87,13 @@
 	
 	# Check the dimensions of the combined data frame.
 	
-	dim(____________________)
+	dim(fish_wide)
 	
 	
 	# Answer in a comment:
 	#
 	# What does bind_rows() do?
-	
+	#Bind rows combines the two data frames and all the rows into a larger data set.
 	
 	
 	# -------------------------------------------------------------------------
@@ -111,10 +112,10 @@
 	
 	fish_long <- fish_wide |>
 	  pivot_longer(
-	    cols = ____________________,
-	    names_to = ____________________,
-	    values_to = ____________________,
-	    values_drop_na = ____________________
+	    cols = codj:pola,
+	    names_to = "species_stage",
+	    values_to = "count",
+	    values_drop_na = TRUE
 	  )
 	
 	fish_long
@@ -122,16 +123,17 @@
 	
 	# Inspect the result.
 	
-	glimpse(____________________)
+	glimpse(fish_long)
 	
 	
 	# Answer in comments:
 	#
 	# 1. What does one row represent before the pivot?
-	#
+	#One row represented the year, site, region, and species.
 	# 2. What does one row represent after the pivot?
-	#
+	#Now one row represents the year, site, region, species, and count.
 	# 3. Why does species_stage still violate the rules of tidy data?
+	#It still contains two variables.
 	
 	
 	
@@ -149,10 +151,10 @@
 	
 	fish_tidy <- fish_long |>
 	  separate_wider_regex(
-	    ____________________,
+	    species_stage,
 	    patterns = c(
-	      species = ____________________,
-	      life_stage = ____________________
+	      species = "[a-z]{3}",
+	      life_stage = "[ja]"
 	    )
 	  )
 	
@@ -165,14 +167,14 @@
 	  mutate(
 	    species = recode(
 	      species,
-	      cod = ____________________,
-	      had = ____________________,
-	      pol = ____________________
+	      cod = "atlantic cod",
+	      had = "haddock",
+	      pol = "pollock"
 	    ),
 	    life_stage = recode(
 	      life_stage,
-	      j = ____________________,
-	      a = ____________________
+	      j = "juvenile",
+	      a = "adult"
 	    )
 	  )
 	
@@ -182,7 +184,7 @@
 	# Answer in a comment:
 	#
 	# What does one row represent in fish_tidy?
-	
+	#One row represents a count of fish species measure during a certain year, region, and site seperated by life stage.
 	
 	
 	# -------------------------------------------------------------------------
@@ -195,11 +197,11 @@
 	
 	fish_summary <- fish_tidy |>
 	  group_by(
-	    ____________________,
-	    ____________________
+	    species,
+	    life_stage
 	  ) |>
 	  summarize(
-	    total_count = sum(____________________),
+	    total_count = sum(count),
 	    .groups = "drop" # optional, removes any groupings that may affect later code
 	  )
 	
@@ -214,8 +216,8 @@
 	)
 	
 	write_csv(
-	  ____________________,
-	  ____________________
+	  fish_tidy,
+	  output_file
 	)
 	
 	file.exists(output_file)
@@ -224,8 +226,9 @@
 	# Answer in comments:
 	#
 	# 1. Which species had the largest total count?
-	#
+	#Pollock had the largest total count.
 	# 2. Were more juveniles or adults observed overall?
+	#More juveniles were observed overall.
 	
 	
 	
@@ -240,7 +243,10 @@
 	# Pollock      | Pollachius virens
 	
 	species_key <- tribble(
-	  ______________________________
+	  ~common_name,   ~scientific_name, 
+	  "atlantic cod", "Gadus morhua", 
+	  "haddock",      "Melanogrammus aeglefinus", 
+	  "polluck",      "Pollachius virens"
 	)
 	
 	species_key
